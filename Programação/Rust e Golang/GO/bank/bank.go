5ed911fcd9c3be775a597dd10622c062e0ba4bd6
= <=>
2025-01-13 13:58:16 -0300

    att

--- Programação/Rust e Golang/GO/bank/bank.go
+++ Programação/Rust e Golang/GO/bank/bank.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	var accountBalance float64 = 1000
+	var accountBalance float64
 
 	fmt.Println("Welcome to GoBank!")
 
@@ -19,36 +19,39 @@ func main() {
 		fmt.Print("Select Your Choice: ")
 		fmt.Scan(&choice)
 
-		if choice == 1 {
+		switch choice {
+		case 1 :
 			fmt.Println("Your Balance is: $", accountBalance)
-		} else if choice == 2 {
+		case 2 :
 			fmt.Print("Your Deposit is: ")
 			var depositAmount float64
 			fmt.Scan(&depositAmount)
 			if depositAmount <= 0 {
 				fmt.Println("Invalid Amount. Must be greater than 0.")
-				return
+				continue
 			}
 			accountBalance += depositAmount
 			fmt.Println("Balance Updated! New Amount: ", accountBalance)
-		} else if choice == 3 {
+		case 3 :
 			fmt.Print("Withdrawal Amount: ")
 			var withdrawalAmount float64
 			fmt.Scan(&withdrawalAmount)
 			if withdrawalAmount <= 0 {
 				fmt.Println("Invalid Amount. Must be greater than 0.")
-				return
+				continue
 			}
 			if withdrawalAmount > accountBalance {
 				fmt.Println("Invalid Amount. You can't withdraw more than you have.")
-				return
+				continue
 			}
 			accountBalance -= withdrawalAmount
 			fmt.Println("Balance Updated! New Amount: ", accountBalance)
-		} else {
-			fmt.Println("Goodbye!")
+		case 4 :
+			fmt.Println("Thanks for choosing our Bank! Goodbye!")
 			return
+		default :
+			fmt.Println("Please put a valid number.")
+			break
 		}
 	}
-
 }
