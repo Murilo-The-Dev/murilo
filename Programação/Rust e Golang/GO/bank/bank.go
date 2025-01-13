ed24951767d7f8373a973a4920dc63d5c9352ce8
= <=>
2025-01-12 23:12:25 -0300

    att

---
+++ Programação/Rust e Golang/GO/bank/bank.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println("Welcome to GoBank!")
+	fmt.Println("What do you want to do?")
+	fmt.Println("1. Check Balance")
+	fmt.Println("2. Deposit Money")
+	fmt.Println("3. Withdraw Money")
+	fmt.Println("4. Exit App")
+
+	var choice int
+
+	fmt.Print("Your Choice:")
+	fmt.Scan(&choice)
+	fmt.Println("Your Choice:", choice)
+
+}
