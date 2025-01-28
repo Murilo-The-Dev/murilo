0f5d5c89f78c20bb91297a6866fd41b1d447cd98
= <=>
2025-01-27 22:04:43 -0300

    att note.go

--- Programação/Rust e Golang/GO/note/main.go
+++ Programação/Rust e Golang/GO/note/main.go
@@ -2,14 +2,33 @@ package main
 
 import (
 	"bufio"
-	"exemple.com/note/note"
 	"fmt"
 	"os"
 	"strings"
+
+	"exemple.com/note/note"
+	"exemple.com/note/todo"
 )
+
+type saver interface {
+	Save() error
+}
+
+type outputtable interface {
+	saver
+	Display()
+}
 
 func main() {
 	title, content := getNoteData()
+	todoText := getUserInput("To Do text: ")
+
+	todo, err := todo.New(todoText)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 
 	userNote, err := note.New(title, content)
 
@@ -18,15 +37,33 @@ func main() {
 		return
 	}
 
-	userNote.Display()
-	err = userNote.Save()
+	err = outputData(todo)
 
 	if err != nil {
-		fmt.Println("saving the note failed")
 		return
 	}
 
+	err = outputData(userNote)
+
+	if err != nil {
+		return
+	}
+}
+func outputData(data outputtable) error {
+	data.Display()
+	return saveData(data)
+}
+
+func saveData(data saver) error {
+	err := data.Save()
+
+	if err != nil {
+		fmt.Println("saving the note failed")
+		return err
+	}
+
 	fmt.Println("saving the note succeeded")
+	return nil
 }
 
 func getNoteData() (string, string) {
