fe21dda48e413c04163c4b33eba89c9d11ccb1e4
= <=>
2025-02-20 21:41:16 -0300

    arrumei tudo e deixei bunitinho

--- Programação/Rust e Golang/PROJETO_GO/teste_1/GUI.go
+++ Programação/Rust e Golang/PROJETO_GO/teste_1/GUI.go
@@ -3,17 +3,18 @@ package main
 import (
 	"fmt"
 	"net/url"
+	"sort"
 	"strconv"
 	"strings"
 	"time"
 
 	"exemple.com/teste_1/product"
-	"fyne.io/fyne/v2/layout"
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/canvas"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/dialog"
+	"fyne.io/fyne/v2/layout"
 	"fyne.io/fyne/v2/theme"
 	"fyne.io/fyne/v2/widget"
 )
@@ -91,6 +92,51 @@ func StartGUI() {
 		})
 		signature.Alignment = fyne.TextAlignCenter
 		return signature
+	}
+
+	createHomeScreen = func() fyne.CanvasObject {
+		title := canvas.NewText("Controle de Estoque", nil)
+		title.TextStyle = fyne.TextStyle{Bold: true}
+		title.TextSize = 24
+		title.Alignment = fyne.TextAlignCenter
+		newProductButton := widget.NewButtonWithIcon("Novo Produto", theme.DocumentCreateIcon(), func() {
+			myWindow.SetContent(createAddProductScreen())
+		})
+		viewProductsButton := widget.NewButtonWithIcon("Ver Produtos", theme.ListIcon(), func() {
+			myWindow.SetContent(createViewProductsScreen())
+		})
+		editProductButton := widget.NewButtonWithIcon("Editar Produto", theme.DocumentCreateIcon(), func() {
+			myWindow.SetContent(createEditProductScreen())
+		})
+		deleteProductButton := widget.NewButtonWithIcon("Excluir Produto", theme.DeleteIcon(), func() {
+			myWindow.SetContent(createDeleteProductScreen())
+		})
+		dashboardButton := widget.NewButtonWithIcon("Dashboard", theme.InfoIcon(), func() {
+			myWindow.SetContent(createDashboardScreen())
+		})
+		exitButton := widget.NewButtonWithIcon("Sair do App", theme.CancelIcon(), func() {
+			myApp.Quit()
+		})
+
+		buttons := container.NewGridWithColumns(
+			2,
+			newProductButton,
+			viewProductsButton,
+			editProductButton,
+			deleteProductButton,
+			dashboardButton,
+			exitButton,
+		)
+
+		content := container.NewVBox(
+			title,
+			widget.NewLabel(""),
+			buttons,
+			widget.NewLabel(""),
+			createSignature(),
+		)
+
+		return container.NewCenter(content)
 	}
 
 	createAddProductScreen = func() fyne.CanvasObject {
@@ -211,55 +257,162 @@ func StartGUI() {
 		return container.NewCenter(content)
 	}
 
-	createHomeScreen = func() fyne.CanvasObject {
-		title := canvas.NewText("Controle de Estoque", nil)
-		title.TextStyle = fyne.TextStyle{Bold: true}
-		title.TextSize = 24
-		title.Alignment = fyne.TextAlignCenter
-		newProductButton := widget.NewButtonWithIcon("Novo Produto", theme.DocumentCreateIcon(), func() {
-			myWindow.SetContent(createAddProductScreen())
-		})
-		viewProductsButton := widget.NewButtonWithIcon("Ver Produtos", theme.ListIcon(), func() {
-			myWindow.SetContent(createViewProductsScreen())
-		})
-		editProductButton := widget.NewButtonWithIcon("Editar Produto", theme.DocumentCreateIcon(), func() {
-			myWindow.SetContent(createEditProductScreen())
-		})
-		deleteProductButton := widget.NewButtonWithIcon("Excluir Produto", theme.DeleteIcon(), func() {
-			myWindow.SetContent(createDeleteProductScreen())
-		})
-		dashboardButton := widget.NewButtonWithIcon("Dashboard", theme.InfoIcon(), func() {
-			myWindow.SetContent(createDashboardScreen())
-		})
-		exitButton := widget.NewButtonWithIcon("Sair do App", theme.CancelIcon(), func() {
-			myApp.Quit()
-		})
+	createEditProductScreen = func() fyne.CanvasObject {
+		
+		myWindow.Resize(fyne.NewSize(800, 600))
+		fieldSize := fyne.NewSize(600, 40)
+		descriptionSize := fyne.NewSize(600, 80)
 
-		buttons := container.NewGridWithColumns(
-			2,
-			newProductButton,
-			viewProductsButton,
-			editProductButton,
-			deleteProductButton,
-			dashboardButton,
-			exitButton,
+		editIDEntry := widget.NewEntry()
+		editIDEntry.SetPlaceHolder("Digite o ID do produto a editar")
+		editIDContainer := container.NewGridWrap(fyne.NewSize(250, 40), editIDEntry)
+	
+		editNameEntry := widget.NewEntry()
+		editNameEntry.SetPlaceHolder("Deixe em branco para manter o valor atual")
+		editNameContainer := container.NewGridWrap(fieldSize, editNameEntry)
+	
+		editQuantityEntry := widget.NewEntry()
+		editQuantityEntry.SetPlaceHolder("Deixe em branco para manter o valor atual")
+		editQuantityContainer := container.NewGridWrap(fyne.NewSize(300, 40), editQuantityEntry)
+	
+		editPriceEntry := widget.NewEntry()
+		editPriceEntry.SetPlaceHolder("Deixe em branco para manter o valor atual")
+		editPriceContainer := container.NewGridWrap(fyne.NewSize(300, 40), editPriceEntry)
+	
+		editCategoryEntry := widget.NewEntry()
+		editCategoryEntry.SetPlaceHolder("Deixe em branco para manter o valor atual")
+		editCategoryContainer := container.NewGridWrap(fyne.NewSize(300, 40), editCategoryEntry)
+	
+		editDescriptionEntry := widget.NewMultiLineEntry()
+		editDescriptionEntry.SetPlaceHolder("Deixe em branco para manter o valor atual")
+		editDescriptionContainer := container.NewGridWrap(descriptionSize, editDescriptionEntry)
+	
+		editSupplierEntry := widget.NewEntry()
+		editSupplierEntry.SetPlaceHolder("Deixe em branco para manter o valor atual")
+		editSupplierContainer := container.NewGridWrap(fyne.NewSize(400, 40), editSupplierEntry)
+	
+		editLocationEntry := widget.NewEntry()
+		editLocationEntry.SetPlaceHolder("Deixe em branco para manter o valor atual")
+		editLocationContainer := container.NewGridWrap(fieldSize, editLocationEntry)
+	
+		instructions := widget.NewLabel(
+			"Preencha apenas os campos que deseja alterar. Campos deixados em branco manterão o valor atual.",
 		)
+		instructions.Wrapping = fyne.TextWrapWord
 
+		validateAndUpdateProduct := func() error {
+			if editIDEntry.Text == "" {
+				return fmt.Errorf("o campo ID é obrigatório")
+			}
+	
+			id, err := strconv.Atoi(editIDEntry.Text)
+			if err != nil {
+				return fmt.Errorf("id deve ser um número inteiro")
+			}
+	
+			products := updateProductList()
+			var productToEdit *product.Product
+			var index int
+	
+			for i, p := range products {
+				if p.PId == id {
+					productToEdit = &products[i]
+					index = i
+					break
+				}
+			}
+	
+			if productToEdit == nil {
+				return fmt.Errorf("produto com id %d não encontrado", id)
+			}
+	
+			if editNameEntry.Text != "" {
+				productToEdit.PName = editNameEntry.Text
+			}
+	
+			if editQuantityEntry.Text != "" {
+				quantity, err := strconv.Atoi(editQuantityEntry.Text)
+				if err != nil {
+					return fmt.Errorf("quantidade deve ser um número inteiro")
+				}
+				productToEdit.PQuantity = quantity
+			}
+	
+			if editPriceEntry.Text != "" {
+				price, err := parsePrice(editPriceEntry.Text)
+				if err != nil {
+					return err
+				}
+				productToEdit.PPrice = price
+			}
+	
+			if editCategoryEntry.Text != "" {
+				productToEdit.PCategory = editCategoryEntry.Text
+			}
+	
+			if editDescriptionEntry.Text != "" {
+				productToEdit.PDescription = editDescriptionEntry.Text
+			}
+	
+			if editSupplierEntry.Text != "" {
+				productToEdit.PSupplier = editSupplierEntry.Text
+			}
+	
+			if editLocationEntry.Text != "" {
+				productToEdit.PLocation = editLocationEntry.Text
+			}
+	
+			productToEdit.UpdatedAt = time.Now()
+	
+			updatedProducts, err := productToEdit.EditProduct(index, products)
+			if err != nil {
+				return fmt.Errorf("erro ao editar produto: %v", err)
+			}
+	
+			return product.SaveProductsToFile(updatedProducts)
+		}
+	
+		editButton := widget.NewButton("Editar Produto", func() {
+			if err := validateAndUpdateProduct(); err != nil {
+				dialog.ShowError(err, myWindow)
+				return
+			}
+			dialog.ShowInformation("Sucesso", "Produto editado com sucesso!", myWindow)
+			myWindow.SetContent(createHomeScreen())
+		})
+	
+		backButton := widget.NewButton("Voltar", func() {
+			myWindow.SetContent(createHomeScreen())
+		})
+	
+		form := container.New(layout.NewFormLayout(),
+			widget.NewLabel("ID do Produto:"), editIDContainer,
+			widget.NewLabel("Novo Nome:"), editNameContainer,
+			widget.NewLabel("Nova Quantidade:"), editQuantityContainer,
+			widget.NewLabel("Novo Preço:"), editPriceContainer,
+			widget.NewLabel("Nova Categoria:"), editCategoryContainer,
+			widget.NewLabel("Nova Descrição:"), editDescriptionContainer,
+			widget.NewLabel("Novo Fornecedor:"), editSupplierContainer,
+			widget.NewLabel("Nova Localização:"), editLocationContainer,
+		)
+	
 		content := container.NewVBox(
-			title,
-			widget.NewLabel(""),
-			buttons,
-			widget.NewLabel(""),
+			widget.NewLabel("Editar Produto"),
+			instructions,
+			container.NewPadded(form),
+			container.NewHBox(layout.NewSpacer(), backButton, editButton, layout.NewSpacer()),
 			createSignature(),
 		)
-
+	
 		return container.NewCenter(content)
 	}
 
 	createViewProductsScreen = func() fyne.CanvasObject {
 		products := updateProductList()
-
+	
 		productList := widget.NewLabel("")
+		productList.Wrapping = fyne.TextWrapWord 
+	
 		sortCriteria := widget.NewSelect([]string{
 			"Nome (A-Z)", "Nome (Z-A)",
 			"ID (Menor-Maior)", "ID (Maior-Menor)",
@@ -289,168 +442,77 @@ func StartGUI() {
 			case "Data de Criação (Antigo-Recente)":
 				product.SortProductsByCreationDate(products, true)
 			}
-
 			productList.SetText(formatProductList(products))
 		})
 		sortCriteria.PlaceHolder = "Ordenar por..."
-
+	
 		backButton := widget.NewButton("Voltar", func() {
 			myWindow.SetContent(createHomeScreen())
 		})
-
-		productList.SetText(formatProductList(products))
-
-		scrollContainer := container.NewVScroll(
+		
+		refreshButton := widget.NewButton("Atualizar", func() {
+			myWindow.SetContent(createViewProductsScreen())
+		})
+	
+		content := container.NewBorder(
 			container.NewVBox(
-				widget.NewLabel("Lista de Produtos"),
+				widget.NewLabelWithStyle("📦 Lista Completa de Produtos", fyne.TextAlignCenter, fyne.TextStyle{Bold: true}),
 				sortCriteria,
-				productList,
 			),
-		)
-
-		return container.NewBorder(
+			container.NewVBox(
+				container.NewHBox(layout.NewSpacer(), refreshButton, backButton, layout.NewSpacer()),
+				createSignature(),
+			),
 			nil,
-			container.NewVBox(backButton, createSignature()),
 			nil,
-			nil,
-			scrollContainer,
-		)
-	}
-
-	createEditProductScreen = func() fyne.CanvasObject {
-		editIDEntry := widget.NewEntry()
-		editNameEntry := widget.NewEntry()
-		editQuantityEntry := widget.NewEntry()
-		editPriceEntry := widget.NewEntry()
-		editCategoryEntry := widget.NewEntry()
-		editDescriptionEntry := widget.NewEntry()
-		editSupplierEntry := widget.NewEntry()
-		editLocationEntry := widget.NewEntry()
-
-		instructions := widget.NewLabel(
-			"Preencha apenas os campos que deseja alterar. Campos deixados em branco manterão o valor atual.",
-		)
-		instructions.Wrapping = fyne.TextWrapWord
-
-		validateAndUpdateProduct := func() error {
-			id, err := strconv.Atoi(editIDEntry.Text)
-			if err != nil {
-				return fmt.Errorf("id deve ser um número inteiro")
-			}
-
-			products := updateProductList()
-			var productToEdit *product.Product
-			var index int
-
-			for i, p := range products {
-				if p.PId == id {
-					productToEdit = &products[i]
-					index = i
-					break
-				}
-			}
-
-			if productToEdit == nil {
-				return fmt.Errorf("produto com id %d não encontrado", id)
-			}
-
-			if editNameEntry.Text != "" {
-				productToEdit.PName = editNameEntry.Text
-			}
-
-			if editQuantityEntry.Text != "" {
-				quantity, err := strconv.Atoi(editQuantityEntry.Text)
-				if err != nil {
-					return fmt.Errorf("quantidade deve ser um número inteiro")
-				}
-				productToEdit.PQuantity = quantity
-			}
-
-			if editPriceEntry.Text != "" {
-				price, err := parsePrice(editPriceEntry.Text)
-				if err != nil {
-					return err
-				}
-				productToEdit.PPrice = price
-			}
-
-			if editCategoryEntry.Text != "" {
-				productToEdit.PCategory = editCategoryEntry.Text
-			}
-			if editDescriptionEntry.Text != "" {
-				productToEdit.PDescription = editDescriptionEntry.Text
-			}
-			if editSupplierEntry.Text != "" {
-				productToEdit.PSupplier = editSupplierEntry.Text
-			}
-			if editLocationEntry.Text != "" {
-				productToEdit.PLocation = editLocationEntry.Text
-			}
-
-			productToEdit.UpdatedAt = time.Now()
-
-			updatedProducts, err := productToEdit.EditProduct(index, products)
-			if err != nil {
-				return fmt.Errorf("erro ao editar produto: %v", err)
-			}
-
-			return product.SaveProductsToFile(updatedProducts)
-		}
-
-		editButton := widget.NewButton("Editar Produto", func() {
-			if err := validateAndUpdateProduct(); err != nil {
-				dialog.ShowError(err, myWindow)
-				return
-			}
-			dialog.ShowInformation("Sucesso", "Produto editado com sucesso!", myWindow)
-			myWindow.SetContent(createHomeScreen())
-		})
-
-		backButton := widget.NewButton("Voltar", func() {
-			myWindow.SetContent(createHomeScreen())
-		})
-
-		return container.NewVBox(
-			widget.NewLabel("Editar Produto"),
-			instructions,
-			widget.NewLabel("ID do Produto a Editar:"), editIDEntry,
-			widget.NewLabel("Novo Nome:"), editNameEntry,
-			widget.NewLabel("Nova Quantidade:"), editQuantityEntry,
-			widget.NewLabel("Novo Preço:"), editPriceEntry,
-			widget.NewLabel("Nova Categoria:"), editCategoryEntry,
-			widget.NewLabel("Nova Descrição:"), editDescriptionEntry,
-			widget.NewLabel("Novo Fornecedor:"), editSupplierEntry,
-			widget.NewLabel("Nova Localização:"), editLocationEntry,
-			editButton,
-			backButton,
-			createSignature(),
-		)
+			container.NewScroll(
+				container.NewPadded(productList),
+		))
+	
+		productList.SetText(formatProductList(products))
+	
+		return content
 	}
 
 	createDeleteProductScreen = func() fyne.CanvasObject {
-		deleteIDEntry := widget.NewEntry()
 
+		deleteIDEntry := widget.NewEntry()
+		deleteIDEntry.SetPlaceHolder("Digite o ID do produto a excluir")
+		deleteIDContainer := container.NewGridWrap(fyne.NewSize(300, 40), deleteIDEntry)
+	
 		deleteButton := widget.NewButton("Excluir Produto", func() {
+			if deleteIDEntry.Text == "" {
+				dialog.ShowError(fmt.Errorf("o campo ID é obrigatório"), myWindow)
+				return
+			}
+	
 			id, err := strconv.Atoi(deleteIDEntry.Text)
 			if err != nil {
 				dialog.ShowError(fmt.Errorf("ID deve ser um número inteiro"), myWindow)
 				return
 			}
-
+	
 			products, err := product.LoadProductsFromFile()
 			if err != nil {
 				dialog.ShowError(fmt.Errorf("erro ao carregar produtos: %v", err), myWindow)
 				return
 			}
-
+	
 			var productName string
+			productFound := false
 			for _, p := range products {
 				if p.PId == id {
 					productName = p.PName
+					productFound = true
 					break
 				}
 			}
-
+	
+			if !productFound {
+				dialog.ShowError(fmt.Errorf("produto com ID %d não encontrado", id), myWindow)
+				return
+			}
+	
 			dialog.ShowConfirm(
 				"Confirmar Exclusão",
 				fmt.Sprintf("Tem certeza que deseja excluir o produto:\nID: %d | Nome: %s?", id, productName),
@@ -467,25 +529,27 @@ func StartGUI() {
 				myWindow,
 			)
 		})
-
+	
 		backButton := widget.NewButton("Voltar", func() {
 			myWindow.SetContent(createHomeScreen())
 		})
-
-		return container.NewVBox(
+	
+		content := container.NewVBox(
 			widget.NewLabel("Excluir Produto"),
-			widget.NewLabel("ID do Produto a Excluir:"),
-			deleteIDEntry,
-			deleteButton,
-			backButton,
+			widget.NewLabel("Digite o ID do produto que deseja excluir:"),
+			deleteIDContainer,
+			container.NewHBox(layout.NewSpacer(), backButton, deleteButton, layout.NewSpacer()),
 			createSignature(),
 		)
+	
+		return container.NewCenter(content)
 	}
 
 	createDashboardScreen = func() fyne.CanvasObject {
+
 		products := updateProductList()
 		stats := product.CalculateDashboardStats(products)
-
+	
 		statsLabel := widget.NewLabel(
 			fmt.Sprintf(
 				"📊 Dashboard de Estoque:\n\n"+
@@ -500,22 +564,46 @@ func StartGUI() {
 				stats.LowestStock.PName, stats.LowestStock.PQuantity,
 			),
 		)
-
-		for category, count := range stats.ProductsByCategory {
+	
+		categories := make([]string, 0, len(stats.ProductsByCategory))
+		for category := range stats.ProductsByCategory {
+			categories = append(categories, category)
+		}
+		sort.Strings(categories)
+	
+		for _, category := range categories {
+			count := stats.ProductsByCategory[category]
 			statsLabel.Text += fmt.Sprintf("- %s: %d\n", category, count)
 		}
-
+	
 		statsLabel.Wrapping = fyne.TextWrapWord
-
+	
 		backButton := widget.NewButton("Voltar", func() {
 			myWindow.SetContent(createHomeScreen())
 		})
-
-		return container.NewVBox(
+	
+		refreshButton := widget.NewButton("Atualizar", func() {
+			myWindow.SetContent(createDashboardScreen())
+		})
+	
+		dashboardContent := container.NewVBox(
+			widget.NewLabelWithStyle("📊 Dashboard de Estoque", fyne.TextAlignCenter, fyne.TextStyle{Bold: true}),
+			widget.NewSeparator(),
 			statsLabel,
-			backButton,
+			widget.NewSeparator(),
+			container.NewHBox(
+				layout.NewSpacer(),
+				backButton,
+				refreshButton,
+				layout.NewSpacer(),
+			),
 			createSignature(),
 		)
+	
+		scrollContainer := container.NewScroll(dashboardContent)
+		scrollContainer.SetMinSize(fyne.NewSize(600, 400))
+	
+		return container.NewPadded(scrollContainer)
 	}
 
 	myWindow.SetContent(createHomeScreen())
