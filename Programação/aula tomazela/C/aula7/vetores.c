ea095be01c0688961bd6cb91b2eaf69f95702d1d
= <=>
2025-04-23 20:16:24 -0300

    vetores c

---
+++ Programação/aula tomazela/C/aula7/vetores.c
@@ -0,0 +1,12 @@
+#include <stdio.h> 
+
+int main() {
+    int numeros[5] = {10, 20, 30, 40, 50};
+
+    printf("Segunda posição: %d\n", numeros[1]);
+    printf("Ultima posição: %d\n\n", numeros[4]);
+    return 0;
+
+    char cidade[30] = "Rio Claro";
+    printf("Cidade: %s\n", cidade);
+}
