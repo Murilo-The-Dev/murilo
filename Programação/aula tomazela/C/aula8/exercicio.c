16733d6ea44082e9121063795ce7197149d56389
= <=>
2025-05-01 10:44:13 -0300

    Organiza código com funções para validação, cálculo e resumo

--- Programação/aula tomazela/C/aula8/exercicio.c
+++ Programação/aula tomazela/C/aula8/exercicio.c
@@ -1,62 +1,73 @@
 #include <stdio.h>
 
-    int main() {
-        int alunos = 0;
-        int qntNotas = 0;
-        float media = 0.0;
-        float mediaSala = 0.0;
-
-        printf("\n~~~~Calculadora de Media das Notas~~~~\n\n");
-        printf("Digite a quantidade de alunos: ");
-        scanf("%d", &alunos);
-        printf("Digite a quantidade de notas por aluno: ");
-        scanf("%d", &qntNotas);
-        printf("\n");
-        float notas[alunos][qntNotas];
-
-        for (int i = 0; i < alunos; i++) {
-            float soma = 0.0;
-            printf("Aluno %d:\n", i + 1);
-            for (int j = 0; j < qntNotas; j++) {
-                printf("  Nota %d: ", j + 1);
-                scanf("%f", &notas[i][j]);
-                soma += notas[i][j];
-            }
-            media = soma / qntNotas;
-            printf("\n  Media do aluno %d: %.2f\n\n", i + 1, media);
-            if (media < 6.0) {
-                printf("  Aluno %d reprovado\n", i + 1);
-            } else {
-                printf("  Aluno %d aprovado\n", i + 1);
-            }
-            printf("  ------------------------------\n");
+float validarNota(int numNota) {
+    float nota;
+    int valid = 0;
+    while (!valid) {
+        printf("  Nota %d: ", numNota);
+        if (scanf("%f", &nota) != 1) {
+            printf("  Entrada invalida! Digite um numero.\n");
+            while (getchar() != '\n');
+        } else if (nota < 0.0 || nota > 10.0) {
+            printf("  Nota invalida! Digite um valor entre 0 e 10.\n");
+        } else {
+            valid = 1;
         }
-
-        printf("\n~~~~Resumo da Sala~~~~\n\n");
-
-        float somaMedias = 0.0;
-        for (int i = 0; i < alunos; i++) {
-            float soma = 0.0;
-            for (int j = 0; j < qntNotas; j++) {
-                soma += notas[i][j];
-            }
-            somaMedias += soma / qntNotas;
-        }
-        mediaSala = somaMedias / alunos;
-        printf("Media da sala: %.2f\n\n", mediaSala);
-
-        int reprovados = 0;
-        for (int i = 0; i < alunos; i++) {
-            float soma = 0.0;
-            for (int j = 0; j < qntNotas; j++) {
-                soma += notas[i][j];
-            }
-            if (soma / qntNotas < 6.0) {
-                reprovados++;
-            }
-        }
-        printf("Quantidade de alunos reprovados: %d\n\n", reprovados);
-        printf("Quantidade de alunos aprovados: %d\n\n", alunos - reprovados);
-
-        return 0;
     }
+    return nota;
+}
+
+float calcularMediaAluno(int qntNotas) {
+    float soma = 0.0;
+    for (int j = 0; j < qntNotas; j++) {
+        soma += validarNota(j + 1);
+    }
+    return soma / qntNotas;
+}
+
+void exibirResumo(int alunos, float medias[], int reprovados) {
+    float somaMedias = 0.0;
+    for (int i = 0; i < alunos; i++) {
+        somaMedias += medias[i];
+    }
+    float mediaSala = somaMedias / alunos;
+
+    printf("\n~~~~Resumo da Sala~~~~\n\n");
+    printf("Quantidade de alunos: %d\n", alunos);
+    printf("Media da sala: %.2f\n", mediaSala);
+    printf("Quantidade de alunos reprovados: %d\n", reprovados);
+    printf("Quantidade de alunos aprovados: %d\n\n", alunos - reprovados);
+}
+
+int main() {
+    int alunos = 0;
+    int qntNotas = 0;
+
+    printf("\n~~~~Calculadora de Media das Notas~~~~\n\n");
+    printf("Digite a quantidade de alunos: ");
+    scanf("%d", &alunos);
+    printf("Digite a quantidade de notas por aluno: ");
+    scanf("%d", &qntNotas);
+    printf("\n");
+
+    float medias[alunos];
+    int reprovados = 0;
+
+    for (int i = 0; i < alunos; i++) {
+        printf("Aluno %d:\n", i + 1);
+        medias[i] = calcularMediaAluno(qntNotas);
+
+        printf("\n  Media do aluno %d: %.2f\n", i + 1, medias[i]);
+        if (medias[i] < 6.0) {
+            printf("  Aluno %d reprovado\n", i + 1);
+            reprovados++;
+        } else {
+            printf("  Aluno %d aprovado\n", i + 1);
+        }
+        printf("  ------------------------------\n");
+    }
+
+    exibirResumo(alunos, medias, reprovados);
+
+    return 0;
+}
