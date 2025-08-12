9ed6ec443594762b6c81a09b014e081dc524a400
= <=>
2025-08-12 18:43:55 -0300

    feat: Implement event management with database integration and API endpoints

--- Programação/Rust e Golang/Maximillian/PROJETO_GO/REST_API/main.go
+++ Programação/Rust e Golang/Maximillian/PROJETO_GO/REST_API/main.go
@@ -3,21 +3,27 @@ package main
 import (
 	"net/http"
 
+	"example.com/REST_API/db"
 	"example.com/REST_API/models"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	db.InitDB()
 	server := gin.Default()
 
-	server.GET("/events",getEvents)
-	server.POST("/events",createEvent)
+	server.GET("/events", getEvents) // GET, POST, PUT, PATCH, DELETE
+	server.POST("/events", createEvent)
 
-	server.Run(":8080") // Localhost:8080
+	server.Run(":8080") // localhost:8080
 }
 
 func getEvents(context *gin.Context) {
-	events := models.GetAllEvents()
+	events, err := models.GetAllEvents()
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not fetch events. Try again later."})
+		return
+	}
 	context.JSON(http.StatusOK, events)
 }
 
@@ -26,11 +32,19 @@ func createEvent(context *gin.Context) {
 	err := context.ShouldBindJSON(&event)
 
 	if err != nil {
-		context.JSON(http.StatusBadRequest, gin.H{"message": "could not parse request data"})
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse request data."})
 		return
 	}
 
 	event.ID = 1
 	event.UserID = 1
-	context.JSON(http.StatusCreated, gin.H{"message": "event created", "event": event})
+
+	err = event.Save()
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not create event. Try again later."})
+		return
+	}
+
+	context.JSON(http.StatusCreated, gin.H{"message": "Event created!", "event": event})
 }
