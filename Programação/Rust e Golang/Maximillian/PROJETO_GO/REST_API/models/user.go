ec9508af7a9fd9b9e5a163255393918a3e7d8213
= <=>
2025-12-15 10:56:23 -0300

    feat: update user registration to hash passwords and add utility for password hashing

--- Programação/Rust e Golang/Maximillian/PROJETO_GO/REST_API/models/user.go
+++ Programação/Rust e Golang/Maximillian/PROJETO_GO/REST_API/models/user.go
@@ -1,6 +1,9 @@
 package models
 
-import "example.com/REST_API/db"
+import (
+	"example.com/REST_API/db"
+	"example.com/REST_API/utils"
+)
 
 type User struct {
 	ID       int64
@@ -18,7 +21,13 @@ func (u User) Save() error {
 
 	defer stmt.Close()
 
-	result, err := stmt.Exec(u.Email, u.Password)
+	hashedPassword, err := utils.HashPassword(u.Password)
+
+	if err != nil {
+		return err
+	}
+
+	result, err := stmt.Exec(u.Email, hashedPassword)
 	if err != nil {
 		return err
 	}
